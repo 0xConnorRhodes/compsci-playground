e67531c9eb436a0dd48cb0ea18857c884122a3c3
Connor Rhodes <connor@rhodes.contact>
2022-11-28 20:53:30 -0600

    added get_string to get user name

--- CS50-intro-to-computer-science/week_1/hello.c
+++ CS50-intro-to-computer-science/week_1/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <cs50.h>
+
+// prompt user for name and greet user by name
 
 int main(void)
 {
+    string name get_string("What is your name?\n");
+
     printf("hello, world\n");
 }
